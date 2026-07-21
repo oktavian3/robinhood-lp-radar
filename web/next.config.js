2788ddbb9e2cd00e86c406adacf644fdfd9b5d15
satyaxbt <satyaxbt@users.noreply.github.com>
2026-07-21 15:17:31 +0200

    fix: default API URL https://api.avetrace.xyz/lp

--- web/next.config.js
+++ web/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   outputFileTracingRoot: path.join(__dirname, '..'),
   experimental: { webpackBuildWorker: false },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:7474',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://api.avetrace.xyz/lp',
   },
 };
 module.exports = nextConfig;
