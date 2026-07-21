7df8473ab37cf87d3dd4b6af267ab12ce63c2bb2
satyaxbt <satyaxbt@users.noreply.github.com>
2026-07-21 15:04:25 +0200

    Fix vercel: remove output export, add vercel.json

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
   outputFileTracingRoot: path.join(__dirname, '..'),
   experimental: { webpackBuildWorker: false },
